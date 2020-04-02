c65e42b70a43405ba4abb2e1120670cafe3bddea
reddrinkbro <ghdwn194@naver.com>
2020-04-02 13:00:26 +0900

    if문 기초

---
+++ Java0402/src/Test2.java
@@ -0,0 +1,31 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		//자바 프로그래밍은 문으로 구성되어있따
+		/*
+		 문 - 1) 실행문 - 순차문
+		 			- 제어문 	- 분기문
+		 					- 반복문
+		 	2) 비실행문 - 주석문 
+		 */
+		//분기문 if
+		/*if(조건식){
+			조건식이 만족할때 실행할 문장
+		}*/
+		// 조건식이 true일 경우 항상 실행된다
+		int num = 100;
+		if(num == 100)
+		{
+			System.out.println("num값은 100이다");
+		}
+		//num값이 0보다 클경우 "양수 입니다." 출력
+		if(num > 0)
+			System.out.println("양수 입니다");
+		// 실행하는 코드의 수가 1줄일때만 중괄호를 생략가능하다
+		
+		
+		
+	}
+
+}
