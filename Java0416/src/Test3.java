02abb19f43569bf44f4439c78176f1aeae26809f
reddrinkbro <ghdwn194@naver.com>
2020-04-17 14:24:14 +0900

    암호 해독, 구구단

--- Java0416/src/Test3.java
+++ Java0416/src/Test3.java
@@ -7,10 +7,20 @@ public class Test3 {
 		
 		//사용자 입력
 		Scanner scan = new Scanner(System.in);
-		//String input = scan.nextLine();
+		String input = "ABDCDS"/*scan.nextLine()*/;
 		
 		char[] charArr = new char[36];
-		char[] charArr1 = new char[36];
+		char[] charArr1 = {
+			'`','~','!','@',
+			'#','$','%','^',
+			'&','*','(',')',
+			'-','_','+','=',
+			'|','[',']','{',
+			'}',';',':',',',
+			'.','/','Q','W',
+			'E','R','T','Y',
+			'U','I','O','P',
+		};
 		char a = 'A';
 		for (int i = 0; i < charArr.length; i++) {
 			charArr[i] = a;
@@ -21,12 +31,25 @@ public class Test3 {
 			charArr[i] = a;
 			a++;
 		}
-		for (int i = 0; i < charArr.length; i++) {
-			System.out.print(charArr[i] + " ");
-		}
 		//암호화 처리
-		
 		//암호화 결과 출력
+		char tmp;
+		for (int i = 0; i < input.length(); i++) {
+			tmp = input.charAt(i);
+			for (int j = 0; j < charArr.length; j++) {
+				if(tmp == charArr[j]) System.out.print(charArr1[j] + " ");
+			}
+		}
+		
+		//복호화
+		/*char tmp;
+		for (int i = 0; i < input.length(); i++) {
+			tmp = input.charAt(i);
+			for (int j = 0; j < charArr1.length; j++) {
+				if(tmp == charArr1[j]) System.out.println(charArr[j]);
+			}
+		}*/
+		
 	}
 
 }
